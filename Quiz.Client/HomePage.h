0c03d1d2a4422f7aa3d2204276cdd04d3bfb9eae
Njabulo Myeni <219051958@stu.ukzn.ac.za>
2024-04-29 21:55:04 +0200

    DragAndDrop

--- Quiz.Client/HomePage.h
+++ Quiz.Client/HomePage.h
@@ -34,8 +34,18 @@ namespace QuizClient {
 				delete components;
 			}
 		}
+
+
+
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
 	protected:
 
 	private:
@@ -51,34 +61,147 @@ namespace QuizClient {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(HomePage::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Impact", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->button1->Location = System::Drawing::Point(215, 323);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(67, 27);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"log in";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Impact", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->button2->Location = System::Drawing::Point(12, 12);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(79, 30);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"Menu";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Impact", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button3->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->button3->Location = System::Drawing::Point(227, 400);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 29);
+			this->button3->TabIndex = 5;
+			this->button3->Text = L"Sign up";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(117, 269);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(288, 31);
+			this->textBox4->TabIndex = 6;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
+			this->label1->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Ink Free", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->label1->Location = System::Drawing::Point(74, 71);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(482, 60);
+			this->label1->TabIndex = 7;
+			this->label1->Text = L"I N N O V A T O R S";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(76, 46);
+			this->textBox1->Location = System::Drawing::Point(117, 189);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
-			this->textBox1->TabIndex = 0;
-			this->textBox1->TextChanged += gcnew System::EventHandler(this, &HomePage::textBox1_TextChanged);
+			this->textBox1->Size = System::Drawing::Size(288, 31);
+			this->textBox1->TabIndex = 8;
 			// 
-			// textBox2
+			// label2
 			// 
-			this->textBox2->Location = System::Drawing::Point(76, 103);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(100, 20);
-			this->textBox2->TabIndex = 1;
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
+			this->label2->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Ink Free", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->label2->Location = System::Drawing::Point(181, 377);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(163, 20);
+			this->label2->TabIndex = 9;
+			this->label2->Text = L"don\'t have an account\?";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
+			this->label3->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Ink Free", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->label3->Location = System::Drawing::Point(122, 166);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(123, 20);
+			this->label3->TabIndex = 10;
+			this->label3->Text = L"Username / email";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
+			this->label4->Cursor = System::Windows::Forms::Cursors::Arrow;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Ink Free", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->ForeColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->label4->Location = System::Drawing::Point(122, 246);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(73, 20);
+			this->label4->TabIndex = 11;
+			this->label4->Text = L"Password";
 			// 
 			// HomePage
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(10, 23);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(534, 261);
-			this->Controls->Add(this->textBox2);
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(621, 460);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Font = (gcnew System::Drawing::Font(L"Impact", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->ForeColor = System::Drawing::SystemColors::ActiveCaption;
+			this->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
 			this->Name = L"HomePage";
 			this->Text = L"HomePage";
+			this->Load += gcnew System::EventHandler(this, &HomePage::HomePage_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -86,5 +209,7 @@ namespace QuizClient {
 #pragma endregion
 	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void HomePage_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
---
+++ Quiz.Client/WriteQuiz.cpp
@@ -0,0 +1,2 @@
+#include "WriteQuiz.h"
+
